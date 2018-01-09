b2b5c9834e1333133b1bd43b301706b618eebcd6
rene <r.van.der.ark@gmail.com>
2018-01-09 20:44:54 +0100

    some notes.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 /* @flow */
 import cast from "flo-cast";
 import { getResizeListeners, initViewPort, getEventListeners } from "resizable-canvas";
+import MusicalScore from "musical-score";
+
 import {
   WebGLRenderer,
   PerspectiveCamera,
@@ -15,7 +17,7 @@ const VIRT_WIDTH = 1000;
 const VIRT_HEIGHT = 1000;
 
 const eventListeners = getEventListeners();
-
+const music = new MusicalScore("https://renevanderark.github.io/arkaic/out/");
 const fooLayer = cast(HTMLCanvasElement, document.getElementById("foo-layer"));
 const renderer = new WebGLRenderer( { canvas: fooLayer } );
 const scene = new Scene();
@@ -59,6 +61,12 @@ eventListeners.add("keydown", (ev, scale) =>  {
     camera.position.z -= 1;
   } else if (key === "ArrowDown") {
     camera.position.z += 1;
+  } else if (key === "a") {
+    music.playNote("piano", "C4h");
+  } else if (key === "s") {
+    music.playNote("piano", "D4h");
+  } else if (key === "d") {
+    music.playNote("piano", "E4h");
   }
 });
 window.setInterval(() => {   cube.rotation.x += 0.1; cube.rotation.y += 0.01; }, 25)
